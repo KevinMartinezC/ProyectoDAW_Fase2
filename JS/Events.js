60e37dc8ff7ec23a48fb2224165d677786ad3748
KevinMartinezC <ceronk003@gmail.com>
2021-10-23 23:09:45 -0600

    first commit

---
+++ JS/Events.js
@@ -0,0 +1,83 @@
+function evento1() {
+  alert(
+    "Ahuachapan\nExtension territoial:244,8 km²\nFecha fiestas patronales:5 al 14 de febrero\nNumero de habitantes:116,948 hab."
+  );
+}
+
+function evento2() {
+  alert(
+    "Cabañas\nExtension territoial:1,103.51 km²\nFecha fiestas patronales:24 de noviembre al 5 de diciembre en honor a Santa Barbara y de 15 de agosto Fiestas de Virgen del transitoNumero de habitantes:215,400 hab."
+  );
+}
+
+function evento3() {
+  alert(
+    "Chalatenango\nExtension territoial:2,016.58 km²\nFecha fiestas patronales:15 a 24 de Junio\nNumero de habitantes:267,201 hab."
+  );
+}
+
+function evento4() {
+  alert(
+    "Cuscatlán\nExtension territoial:756.19 km²\nFecha fiestas patronales:15 al 29 de enero\nNumero de habitantes:232,238 hab."
+  );
+}
+
+function evento5() {
+  alert(
+    "La Libertad\nExtension territoial:1,653 km²\nFecha fiestas patronales:Mes de Diciembre en honor al niño Jesus\nNumero de habitantes:660,653 hab."
+  );
+}
+
+function evento6() {
+  alert(
+    "Morazán\nExtension territoial:1,447 km²\nFecha fiestas patronales:1 al 15 de Octubre\nNumero de habitantes:251,447 hab."
+  );
+}
+
+function evento7() {
+  alert(
+    "La Paz\nExtension territoial:1,223.61 km²\nFecha fiestas patronales:15 al 18 de Agosto\nNumero de habitantes:320,379 hab."
+  );
+}
+
+function evento8() {
+  alert(
+    "Santa Ana\nExtension territoial:408 km²\nFecha fiestas patronales:17 al 26 de julio\nNumero de habitantes:272,554 hab."
+  );
+}
+
+function evento9() {
+  alert(
+    "San Miguel\nExtension territoial:549 km²\nFecha fiestas patronales:14 al 30 de Noviembre\nNumero de habitantes:265,921 hab."
+  );
+}
+
+function evento10() {
+  alert(
+    "Sonsonate\nExtension territoial:232 km²\nFecha fiestas patronales:25 de enero al 2 de febrero\nNumero de habitantes:72,951 hab."
+  );
+}
+
+function evento11() {
+  alert(
+    "San Salvador\nExtension territoial:72 km²\nFecha fiestas patronales:1 agosto al 6 de agosto\nNumero de habitantes:238,244 hab."
+  );
+}
+
+function evento12() {
+  alert(
+    "San Vicente\nExtension territoial:262 km²\nFecha fiestas patronales:Se celebran en el mes de Diciemre\nNumero de habitantes:53,203 hab."
+  );
+}
+
+function evento13() {
+  alert(
+    "La Unión\nExtension territoial:244 km²\nFecha fiestas patronales:primera quincena del mes de Octubre\nNumero de habitantes:34,045 hab."
+  );
+}
+
+function evento14() {
+  alert(
+    "Usulután\nExtension territoial:2,130 km²\nFecha fiestas patronales:19 al 25 de Noviembre en honor a Santa Catarina de Alejandria\nNumero de habitantes:317,079 hab."
+  );
+}
